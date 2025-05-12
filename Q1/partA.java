2f41a64a3fef504546cf8b862d1731435194e5a5
Shanjian2311 <shanjianz@nycstudents.net>
2025-05-12 11:52:42 -0400

    Update partA.java

--- Q1/partA.java
+++ Q1/partA.java
@@ -1,1 +1,19 @@
+public class Main {
+    public static void main(String [] args) {
+        System.out.println("Hello, world!");
+    }
 
+}
+
+public int dogWalkShift(int sHrs, int eHrs){
+    int moneyEarn = 0;
+    for(int hr =sHrs; hr <= eHrs; hr++){
+        int dogs = walkDogs(hr);
+        moneyEarn == dogs*5; // earn $5 per dog walked
+
+        if(dogs = maxDogs || (hr >= 9 && hr <= 17)){
+
+        }
+    }
+    return moneyEarn;
+}
