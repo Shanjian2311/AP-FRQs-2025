cd33102593cff27b2373bcce74860293d7da0e6a
Shanjian2311 <shanjianz@nycstudents.net>
2025-05-22 11:44:41 -0400

    Update Q2.java

--- Q2.java
+++ Q2.java
@@ -1,1 +1,21 @@
+public class SignedText{
+    private String ln = "";
+    private String fn = "";
+    
+    public SignedText(String firstN, String lastN){
+        fn = firstN;
+        ln = lastN;
+    }
 
+    public String getSignature(){
+        return fn.substring(0,1) + "-" + ln;
+    }
+
+    public String addSignature(String text){
+        int locate = text.indexof(getSignature);
+        if(this.text.indexof(getSignature) > 0){
+            return text.substring(locate, (locate + getSignature.length())) + text.substring(locate+getSignature.length());
+        }
+        return text
+    }
+}
